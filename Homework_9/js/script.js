b6a7e6b6d76669c54c8bc72d86f1c03c32c7b1a2
Timur681 <teekirienkov@gmail.com>
2020-03-22 14:00:10 +0300

    Сделал домашнее задание. Это дз можно использовать в будущем

---
+++ Homework_9/js/script.js
@@ -0,0 +1,40 @@
+window.addEventListener('DOMContentLoaded', () => {
+    'use strict';
+
+    let tab = document.querySelectorAll('.info-header-tab'), // Тут табы Лечение, Отдых, Природа, Йога
+        info = document.querySelector('.info-header'), // Тут все табы (которые в tab)
+        tabContent = document.querySelectorAll('.info-tabcontent'); // Информация внутри табов
+
+
+    const hideTabContent = (a) => {
+        for (let i = a; i < tabContent.length; i++ ) {
+            tabContent[i].classList.remove('show');       // Эта функция скрывает все tabContent
+            tabContent[i].classList.add('hide');
+        }
+    };
+    hideTabContent(1);  // Этот вызов (1) функции оставляет только 1 таб (Лечение)
+
+
+    const showTabContent = (b) => {
+        if (tabContent[b].classList.contains('hide')) {
+            tabContent[b].classList.remove('hide');      // Эта функция показывает определенный таб
+            tabContent[b].classList.add('show');
+        }
+    };
+
+
+    // Этот обработчик установлен на все табы! Функция внутри него показывает нужный таб
+    info.addEventListener('click', (event) => {      // Делегирование событий
+        let target = event.target;
+        if (target && target.classList.contains('info-header-tab')) {     // Проверка что клик был на один из табов
+            for (let i = 0; i < tab.length; i++) {        // Цикл работает пока не проверит все табы (tab.length)
+                if (target == tab[i]) {              
+                    hideTabContent(0);            // Это условие выводит нужный таб
+                    showTabContent(i);
+                    break;
+                }
+            }
+        }
+    });
+
+});

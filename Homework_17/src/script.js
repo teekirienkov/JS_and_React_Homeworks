e3e159caefa438cc993a08907fd3777d0d1b6d22
Timur681 <teekirienkov@gmail.com>
2020-04-06 21:05:27 +0300

    search elements on html

--- Homework_17/src/script.js
+++ Homework_17/src/script.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
-	
+	let overlay = $('.overlay'),
+    	modal = $('.modal'),
+    	btnClose = $('.close'),
+    	btnSelect = $('.main_btna'), // Выбрать тур
+    	btnAdvice = $('.main_btn'), // Получить консультацию
+    	btnTimetable = $('.timetable'); // Расписание туров
 });
